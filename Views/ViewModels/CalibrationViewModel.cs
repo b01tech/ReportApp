91d7eedc9b43db7a567a88ece0f2bde3659006b3
b01tech <b01.tech@gmail.com>
2024-08-28 15:34:57 -0300

    feat: add viewmodels

---
+++ Views/ViewModels/CalibrationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ReportApp.Data;
+using ReportApp.Models;
+
+namespace ReportApp.Views.ViewModels;
+
+public class CalibrationViewModel
+{
+    public List<Calibration> Calibrations { get; set; }
+
+    public CalibrationViewModel()
+    {
+        using (var context = new AppDbContext())
+        {            
+            context.Database.EnsureCreated();
+
+            Calibrations = context.Calibrations.ToList();
+        }
+    }
+}
