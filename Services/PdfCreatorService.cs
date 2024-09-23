42b9ec349d82afe761d1583825e3b65581c973d2
b01tech <bruno01.tech@gmail.com>
2024-09-23 16:00:59 -0300

    style: change report pdf layout

--- Services/PdfCreatorService.cs
+++ Services/PdfCreatorService.cs
@@ -6,6 +6,7 @@ using iText.Kernel.Colors;
 using ReportApp.Models;
 using ReportApp.Models.Extensions;
 using System.IO;
+using iText.IO.Image;
 
 
 
@@ -31,72 +32,96 @@ public class PdfCreatorService
         PdfDocument pdf = new PdfDocument(writer);
         Document doc = new Document(pdf, iText.Kernel.Geom.PageSize.A4);
 
+        Image logoDigiRbc = new Image(ImageDataFactory.Create(Path.Combine(imagesPath, "logo-digi-rbc.JPG")));
+        logoDigiRbc.ScaleToFit(300, 550);
+        Image imgEccTypeA = new Image(ImageDataFactory.Create(Path.Combine(imagesPath, "EccTestTypeA.JPG")));
+        imgEccTypeA.ScaleToFit(100, 100);
+        Image imgEccTypeB = new Image(ImageDataFactory.Create(Path.Combine(imagesPath, "EccTestTypeB.JPG")));
+        imgEccTypeB.ScaleToFit(100, 100);
+
         var t1 = new Table(6);
         t1.SetWidth(520);
         t1.SetFontSize(8).SetTextAlignment(TextAlignment.CENTER).SetBold();
 
-        t1.AddCell(new Cell(3, 3).Add(new Paragraph("IMAGEM:")));
-        t1.AddCell(new Cell(1, 3).Add(new Paragraph($"CERTIFICADO")).SetBackgroundColor(ColorConstants.DARK_GRAY).SetFontColor(ColorConstants.WHITE));
+        t1.AddCell(new Cell(3, 3).Add(logoDigiRbc.SetHorizontalAlignment(HorizontalAlignment.CENTER)));
+        t1.AddCell(new Cell(1, 3).Add(new Paragraph($"CERTIFICADO")).SetBackgroundColor(ColorConstants.DARK_GRAY).SetFontColor(ColorConstants.WHITE).SetFontSize(12));
         t1.AddCell(new Cell(2, 3).Add(new Paragraph($"{cal.ReportId}").SetFontSize(12)));
 
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Cliente:")));
-        t1.AddCell(new Cell(1, 5).Add(new Paragraph($"{cal.Customer.Name}").SetHorizontalAlignment( HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Endereço:")));
-        t1.AddCell(new Cell(1, 5).Add(new Paragraph($"{cal.Customer.Address}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Cidade:")));
-        t1.AddCell(new Cell(1, 3).Add(new Paragraph($"{cal.Customer.City}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Estado:")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.Customer.State}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Cliente:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 5).Add(new Paragraph($"{cal.Customer.Name}").SetTextAlignment(TextAlignment.LEFT)));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Endereço:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 5).Add(new Paragraph($"{cal.Customer.Address}").SetTextAlignment(TextAlignment.LEFT)));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Cidade:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 3).Add(new Paragraph($"{cal.Customer.City}").SetTextAlignment(TextAlignment.LEFT)));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Estado:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.Customer.State}").SetTextAlignment(TextAlignment.LEFT)));
         t1.AddCell(new Cell(1, 6).Add(new Paragraph("")));
 
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Fabricante:")));
-        t1.AddCell(new Cell(1, 5).Add(new Paragraph($"{cal.Scale.Model}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Nº de série:")));
-        t1.AddCell(new Cell(1, 5).Add(new Paragraph($"{cal.Scale.SerialNo}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("TAG:")));
-        t1.AddCell(new Cell(1, 3).Add(new Paragraph($"{cal.Scale.TagName}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Classe:")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Fabricante:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 5).Add(new Paragraph($"{cal.Scale.Model}").SetTextAlignment(TextAlignment.LEFT)));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Nº de série:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 5).Add(new Paragraph($"{cal.Scale.SerialNo}").SetTextAlignment(TextAlignment.LEFT)));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("TAG:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 3).Add(new Paragraph($"{cal.Scale.TagName}").SetTextAlignment(TextAlignment.LEFT)));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Classe:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.Scale.ScaleClass.ScaleClassToString()}")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Capacidade:")));
-        t1.AddCell(new Cell(1, 3).Add(new Paragraph($"{cal.Scale.Capacity}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("UNIDADE:")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Capacidade:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 3).Add(new Paragraph($"{cal.Scale.Capacity}").SetTextAlignment(TextAlignment.LEFT)));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("UNIDADE:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.Scale.Unit.ToString()}")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"Resolução:")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"d=")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"Resolução:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"d=")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.Scale.ResolutionD}")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"e=")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"e=")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.Scale.ResolutionE}")));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph("")));
         t1.AddCell(new Cell(1, 6).Add(new Paragraph("")));
 
-        t1.AddCell(new Cell(1, 6).Add(new Paragraph("Ensaio de Mobilidade")));
-        t1.AddCell(new Cell(1, 2).Add(new Paragraph("Leitura sem sobrecarga:")));
-        t1.AddCell(new Cell(1, 2).Add(new Paragraph("Sobrecarga aplicada")));
-        t1.AddCell(new Cell(1, 2).Add(new Paragraph("Leitura após a sobrecarga:")));
+        t1.AddCell(new Cell(1, 6).Add(new Paragraph("Ensaio de Mobilidade")).SetBackgroundColor(ColorConstants.DARK_GRAY).SetFontColor(ColorConstants.WHITE));
+        t1.AddCell(new Cell(1, 2).Add(new Paragraph("Leitura sem sobrecarga:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 2).Add(new Paragraph("Sobrecarga aplicada")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 2).Add(new Paragraph("Leitura após a sobrecarga:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.MobTest.MobBefore}")));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.MobTest.MobLoad}")));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.MobTest.MobAfter}")));
         t1.AddCell(new Cell(1, 6).Add(new Paragraph("")));
 
-        t1.AddCell(new Cell(1, 3).Add(new Paragraph("Ensaio de Repetibilidade")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Peso aplicado:")));
+        t1.AddCell(new Cell(1, 3).Add(new Paragraph("Ensaio de Repetibilidade")).SetBackgroundColor(ColorConstants.DARK_GRAY).SetFontColor(ColorConstants.WHITE));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Peso aplicado:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.RepTest.RepMass}")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Leitura 1:")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Leitura 1:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.RepTest.RepRead1}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Leitura 2:")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Leitura 2:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.RepTest.RepRead2}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Leitura 3:")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Leitura 3:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.RepTest.RepRead2}").SetHorizontalAlignment(HorizontalAlignment.LEFT)));
         t1.AddCell(new Cell(1, 6).Add(new Paragraph("")));
 
-        t1.AddCell(new Cell(1, 6).Add(new Paragraph("Ensaio de Excentricidade")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("TIPO DE ENSAIO:")));
+        t1.AddCell(new Cell(1, 6).Add(new Paragraph("Ensaio de Excentricidade")).SetBackgroundColor(ColorConstants.DARK_GRAY).SetFontColor(ColorConstants.WHITE));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("TIPO DE ENSAIO:")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.EccTest.Type}")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Peso aplicado")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("Peso aplicado")).SetVerticalAlignment(VerticalAlignment.MIDDLE).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.EccTest.EccLoad}{cal.Scale.Unit.UnitToString()}")));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph("A")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.EccTest.A}")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
-        t1.AddCell(new Cell(6, 3).Add(new Paragraph("IMAGEM")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
+        if (cal.EccTest.Type == "Industrial")
+        {
+            t1.AddCell(new Cell(6, 3)
+            .Add(imgEccTypeA.SetHorizontalAlignment(HorizontalAlignment.CENTER))
+            .SetVerticalAlignment(VerticalAlignment.MIDDLE));
+
+        }
+        else if (cal.EccTest.Type == "Rodoviária")
+        {
+            t1.AddCell(new Cell(6, 3).Add(imgEccTypeB.SetHorizontalAlignment(HorizontalAlignment.CENTER)).SetVerticalAlignment(VerticalAlignment.MIDDLE));
+
+        }
+        else
+        {
+            t1.AddCell(new Cell(6, 3).Add(new Paragraph("Não se aplica")).SetVerticalAlignment(VerticalAlignment.MIDDLE).SetHorizontalAlignment(HorizontalAlignment.CENTER));
+
+        }
+
         t1.AddCell(new Cell(1, 1).Add(new Paragraph("B")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.EccTest.B}")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph("C")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
@@ -109,11 +134,11 @@ public class PdfCreatorService
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.EccTest.F}")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
         t1.AddCell(new Cell(1, 6).Add(new Paragraph("")));
 
-        t1.AddCell(new Cell(1, 6).Add(new Paragraph("Teste de Pesagem")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("CARGA")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("LEITURA")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("ERRO")));
-        t1.AddCell(new Cell(1, 3).Add(new Paragraph("PADRÕES DE TRABALHO UTILIZADOS")));
+        t1.AddCell(new Cell(1, 6).Add(new Paragraph("Teste de Pesagem")).SetBackgroundColor(ColorConstants.DARK_GRAY).SetFontColor(ColorConstants.WHITE));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("CARGA")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("LEITURA")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("ERRO")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 3).Add(new Paragraph("PADRÕES DE TRABALHO UTILIZADOS")).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.WeightTest[0].WLoad}")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.WeightTest[0].WRead}")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"000000")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
@@ -133,18 +158,18 @@ public class PdfCreatorService
                 t1.AddCell(new Cell(1, 1).Add(new Paragraph($"--")).SetVerticalAlignment(VerticalAlignment.MIDDLE));
             }
         }
-
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("EXECUTOR")));
+        t1.AddCell(new Cell(3, 6).Add(new Paragraph("- Incerteza expandida baseada em uma incerteza combinada multiplicada por um fator de abrangência k=2, para um nível de confiança de aproximadamente 95%.\r\n- INCERTEZA EXPANDIDA DAS LEITURAS EFETUADAS = 0,04%.").SetTextAlignment(TextAlignment.LEFT)).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("EXECUTOR").SetFontSize(7)).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.Technician}")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("DATA DO ENSAIO:")));
-        t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.DateCal.ToString("dd/MM/yyyy")}")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("RESPONSÁVEL:")));
+        t1.AddCell(new Cell(1, 2).Add(new Paragraph("DATA DO ENSAIO:").SetFontSize(7).SetTextAlignment(TextAlignment.LEFT)).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.DateCal.ToString("dd/MM/yyyy")}")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("RESPONSÁVEL:").SetFontSize(7)).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.Manager}")));
-        t1.AddCell(new Cell(1, 2).Add(new Paragraph("TOLERÂNCIA CONFORME::")));
+        t1.AddCell(new Cell(1, 2).Add(new Paragraph("TOLERÂNCIA CONFORME:").SetFontSize(7).SetTextAlignment(TextAlignment.LEFT)).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph("Portaria 157/22")));
-        t1.AddCell(new Cell(1, 1).Add(new Paragraph("LOCAL:")));
+        t1.AddCell(new Cell(1, 1).Add(new Paragraph("LOCAL:").SetFontSize(7)).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 2).Add(new Paragraph($"{cal.Place}")));
-        t1.AddCell(new Cell(1, 2).Add(new Paragraph("Situação do instrumento:")));
+        t1.AddCell(new Cell(1, 2).Add(new Paragraph("SITUAÇÃO DO INSTRUMENTO:").SetFontSize(7)).SetBackgroundColor(ColorConstants.LIGHT_GRAY));
         t1.AddCell(new Cell(1, 1).Add(new Paragraph($"{cal.Status}")));
         t1.AddCell(new Cell(1, 6).Add(new Paragraph("Procedimento de Referência: PR17-DT-CCD_certificado de Conformidade DIGI-TRON").SetFontSize(6)));
         t1.AddCell(new Cell(1, 6).Add(new Paragraph("Fábrica, Administração, Vendas, Locação e Assistência Técnica. Fone: 41-3377-1577 | E-mail: conformidade@digitronbalancas.com.br").SetFontSize(6)));
